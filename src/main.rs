b5be241babf14b39e0680de619697f114dcbe033
jinxdash <jinxdash.github@gmail.com>
2022-08-27 14:10:26 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -65,6 +65,7 @@ lazy_static! {
         "https://gitlab.com/ttyperacer/terminal-typeracer".to_string(), // GitLab repo with >40 stars.
         "https://github.com/esp-rs".to_string(), // Espressif Rust Organization (Organizations have no stars).
         "https://github.com/arkworks-rs".to_string(), // Rust ecosystem for zkSNARK programming (Organizations have no stars)
+        "https://marketplace.visualstudio.com/items?itemName=jinxdash.prettier-rust".to_string(), // https://github.com/jinxdash/prettier-plugin-rust has >50 stars
     ];
 }
 
