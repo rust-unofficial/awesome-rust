bfba4636c54c6ac4951d581d2a27dc663e1875b7
Tom Parker-Shemilt <palfrey@tevp.net>
2025-07-19 13:19:38 +0100

    Remove nativelink override

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,6 @@ lazy_static! {
         "https://www.gnu.org/software/emacs/",
         "http://www.gnu.org/software/gsl/",
         "https://labex.io/skilltrees/rust",
-        "https://github.com/TraceMachina/nativelink", // probably broken because @palfrey now works for them...
         "https://www.vulkan.org/",
     ].iter().map(|s| s.to_string()).collect();
     // Overrides for popularity count, each needs a good reason (i.e. downloads/stars we don't support automatic counting of)
